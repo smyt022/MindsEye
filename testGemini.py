c902ec086901209999035cec587063c42fa31c06
smyt022 <tocatsa@gmail.com>
2025-02-25 16:13:38 -0500

    made basic game with hardcoded word

--- testGemini.py
+++ testGemini.py
@@ -9,9 +9,26 @@ def get_Gemini_key():
 #setup client
 client = genai.Client(api_key=get_Gemini_key())
 
-prompt = input("Enter prompt: ")
+#globals
+word = "elephant"
+successfulGuess = False
+numOfGuesses = 0
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash", contents="in 4 sentences..."+prompt
-)
-print(response.text)
+for i in range(3):
+    prompt = input("ask a True or False question: ")
+    response = client.models.generate_content(
+        model="gemini-2.0-flash", contents="Answer only True or False to the following question/statement, do NOT say anything else {"+prompt+"}"
+    )
+    print(response.text)
+    prompt = input("Guess the word: ")
+    numOfGuesses += 1
+    if(prompt.lower() == word.lower()):
+        successfulGuess = True
+        print("correct!")
+        break
+    else:
+        successfulGuess = False
+        print("incorrect!")
+        
+
+
